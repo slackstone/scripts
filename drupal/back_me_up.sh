4dc559be2578e224be3b1adcca7c640086ab4fb6
Stephen Groundwater <Stephen.groundwater@gmail.com>
2021-12-01 20:56:08 -0500

    Update back_me_up.sh

--- drupal/back_me_up.sh
+++ drupal/back_me_up.sh
@@ -1,47 +1,72 @@
 #!/bin/bash
-# Backup - MySQL and Static Site Files
-
-# sitename and path
-sitename="example.com"
-sitepath="/opt/www/example.com/web"
-privatefiles="/opt/www/example.com/private"
-databasename="DB_NAME"
-dbpassword="DB_PASS"
-EMAIL="admin@example.com"
-EMAILMESSAGE="msg.txt"
-backup_path="/opt/backup/"
-
-# File Name Setup
+# Drupal 9 Backup Script, Grabs MySQL DB and Static Site Files
+# Setup variables
+echo "Setup Parameters"
+echo "1 = Site Name"
+echo "2 = Site Path"
+echo "3 = Location For Backups"
+echo "Example: ./back_me_up.sh gluebox.com /opt/www/gluebox.com /opt/backup"
+echo ""
+sitename=$1
+sitepath=$2
+backup_path=$3
+#check if we got parameters
+if [[ $# -eq 0 ]] ; then
+    echo 'Error Missing Arguments'
+    echo "Example: ./back_me_up.sh gluebox.com /opt/www/gluebox.com /opt/backup"
+    exit 1
+fi
+# TODO; account for private files
+# privatefiles="/opt/www/$sitename/private"
+# Find the Drupal DB Name
+dbname="$(cat $sitepath/web/sites/default/settings.php | grep database\' | grep -v \* | grep -v \#)"
+dbname="${dbname//\'}"
+dbname="${dbname//database \=\> }"
+dbname="${dbname//\,}"
+dbname="${dbname// }"
+echo "DB Name:"$dbname
+## Find the Drupal DB User
+dbuser="$(cat $sitepath/web/sites/default/settings.php | grep username | grep -v \* | grep -v \# )"
+dbuser="${dbuser//\'username\'}"
+dbuser="${dbuser// => }"
+dbuser="${dbuser// \'}"
+dbuser="${dbuser// }"
+dbuser="${dbuser//\',}"
+echo "DB User Name:"$dbuser
+## Find the Drupal DB Password
+dbpassword="$(cat $sitepath/web/sites/default/settings.php | grep pass | grep -v \* | grep -v \#)"
+dbpassword="${dbpassword//\'}"
+dbpassword="${dbpassword//\,}"
+dbpassword="${dbpassword//=> }"
+dbpassword="${dbpassword// }"
+dbpassword="${dbpassword//password}"
+echo "DB Password: DO NOT DISPLAY"
+echo "Backup Path:"$backup_path
+## File Name Setup
 NOW=$(date +"%Y%m%d")
+echo "The time is now:"$NOW
 STATIC_FILES="$sitename.FILES.$NOW.tgz"
+echo "Static site files will go here:"$STATIC_FILES
 SQL_FILES="$sitename.SQL.$NOW.sql"
-SQL_ZIP="$sitename.SQL.$NOW.tgz"
-
-# Where to place files
+# Check for backup directory and log files
+echo "Check if backup dir exists, else make it."
+fullbackup_path=$3"/"$1
+if [ -d "$fullbackup_path" ]; then
+  echo "${fullbackup_path} was found."
+else
+  echo "WARNING: ${fullbackup_path} was not found. Adding new backup subfolder"
+  mkdir $fullbackup_path
+fi
+echo "Check if a backup log file exists, else make it."
+if [ -f "${fullbackup_path}/backup.log" ]; then
+  echo "${fullbackup_path}/backup.log was found."
+else
+  echo "WARNING: ${fullbackup_path}/backup.log was not found. Adding new backup logfile"
+  touch $fullbackup_path/backup.log
+fi
 # Backup and Zip Static Site Files
-tar cvfz $backup_path/$sitename/$STATIC_FILES $sitepath $privatefiles 2>tar_error.txt
-echo "$sitename Static Site Backup Status" >> $EMAILMESSAGE
-echo "------------------------" >> $EMAILMESSAGE
-cat tar_error.txt >> $EMAILMESSAGE
-echo "------------------------" >> $EMAILMESSAGE
+tar cfz $fullbackup_path/$STATIC_FILES $sitepath 2>> $fullbackup_path/backup.log
 #Backup MySQL Databases
-mysqldump -u phpmyadmin -p$dbpassword $databasename > $backup_path/$sitename/$SQL_FILES 2>>sql_error.txt
-#Zip SQL Files
-tar cvfz $backup_path/$sitename/$SQL_ZIP $backup_path/$sitename/*.sql 2>>tar_error.txt
-#Cleanup SQL files
-rm $backup_path/$sitename/*.sql
-echo "$sitename SQL Backup Status" >> $EMAILMESSAGE
-cat sql_error.txt >> $EMAILMESSAGE
-echo "-------------------------">> $EMAILMESSAGE
-# Test for command results and drop in tmp file
-date >> $EMAILMESSAGE
-echo $? >> $EMAILMESSAGE
-echo "Date / Time Run" >>$EMAILMESSAGE
-echo "--------------------------">> $EMAILMESSAGE
-#Email Results of Backup
-SUBJECT="$sitename Site Backups"
-# send an email using /bin/mail
-# Adjust your tools as needed
-/usr/bin/mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
-# Cleanup Tmp Fies
-rm msg.txt
+mysqldump -u $dbuser -p$dbpassword $dbname > $fullbackup_path/$SQL_FILES 2>>$fullbackup_path/backup.log
+echo "Backup Run Complete. Here are the details:"
+ls -lah $fullbackup_path
